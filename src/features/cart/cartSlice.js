ba58d8ffcf2faa2579f91f19db27019122ce38de
FikretDuzceker <fikretduzceker5@gmail.com>
2022-10-13 09:57:09 +0300

    first commit

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
+const initialState = {
+  cartItems: cartItems,
+  amount: 1,
+  total: 0,
+  isLoading: true,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+  },
+});
+export const { clearCart } = cartSlice.actions;
+console.log(cartSlice);
+export default cartSlice.reducer;
